ed4caa25d4ce22086205e7ee28633a072ce6af31
KennethBlandon <kennethb@uamv.edu.ni>
2024-06-11 11:34:29 -0600

    Avance

--- Sesion0706/Funciones.cpp
+++ Sesion0706/Funciones.cpp
@@ -15,6 +15,8 @@ CIUDAD buscar(int ID);
 int menu();
 void principal();
 void pedirDatos();
+void MostrarDatos();
+void buscarxID();
 
 void agregar(CIUDAD *c)
 {
@@ -24,6 +26,8 @@ void agregar(CIUDAD *c)
 
 CIUDAD buscar(int ID)
 {
+    CIUDAD c ;
+    c.ID = 0;
     for (int i = 0; i < pos; i++)
     {
         if (ID == ciudades[i].ID)
@@ -31,9 +35,11 @@ CIUDAD buscar(int ID)
             return ciudades[i];
         }
     }
-    CIUDAD c;
+    
     return c;
+    
 }
+
 int obtPos(int ID)
 {
     for (int i = 0; i < pos; i++)
@@ -67,16 +73,16 @@ int menu()
     int op;
     cout << "Menu \n";
     cout << "1.Agregar\n";
-    cout << "2. Editar\n";
+    cout << "2.Editar\n";
     cout << "3.Eliminar\n";
     cout << "4.Buscar\n";
     cout << "5.Mostrar Todo\n";
-    cout << "6. Salir\n";
+    cout << "6.Salir\n";
     cout << "Digite la opcion\n";
     cin >> op;
     return op;
 }
-void principa()
+void principal()
 {
     int op;
     do
@@ -87,6 +93,12 @@ void principa()
         case 1:
             pedirDatos();
             break;
+        case 4:
+            buscarxID();
+            break;
+        case 5:
+        MostrarDatos();
+        break;
         case 6:
             cout << "Adios, Mi tierno\n";
             break;
@@ -104,9 +116,31 @@ void pedirDatos()
     cout << "ID: ";
     cin >> ciudad.ID;
     cout << "Nombre: ";
-    cin >> ciudad.Nombre;
+    scanf(" %[^\n]", ciudad.Nombre);
     cout << "Descripcion: ";
-    cin >> ciudad.Descripcion;
+    scanf(" %[^\n]", ciudad.Descripcion);
     agregar(&ciudad);
     cout << "Registro agregado...\n";
+}
+
+void MostrarDatos(){
+    for (int i = 0; i<pos; i++){
+        cout << "ID" << ciudades[i].ID << endl;
+        cout << "Nombre: " << ciudades[i].Nombre << endl;
+        cout << "Descripcion: " << ciudades[i].Descripcion << endl;
+    }
+}
+
+void buscarxID(){
+    int ID;
+    cout << "Dime el ID de la ciudad a buscar: ";
+    cin >> ID;
+    CIUDAD c;
+    c = buscar(ID);
+    cout << "============================" << endl;
+    cout << c.ID << endl;
+    cout << c.Nombre << endl;
+    cout << c.Descripcion << endl;
+    cout << "============================" << endl;
+
 }
